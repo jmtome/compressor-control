6c7534ed90b9687664b84ecdd2562af04ad0b11b
jmtome <jtome@fi.uba.ar>
2025-06-11 18:39:59 -0300

    Added Libraries and GPIO
    Added GPIO for LCD and also ADDED Library  LiquidCrystal_I2C by Frank de Brabander.
    
    Added Input/Output for analog sensors and digital outputs.

--- compressor-control/compressor-control.ino
+++ compressor-control/compressor-control.ino
@@ -1,3 +1,24 @@
+// === Libraries ===
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+// === Constants ===
+#define COMPRESSOR_START_PIN 6
+#define COMPRESSOR_ON_PIN 7
+
+#define NTC_PIN A0
+#define VACUUM_SENSOR_PIN A1
+
+// Comment-Uncomment desired or check datasheet
+#define SHT30_ADDRESS 0x44   //Default wiring (ADDR = GND or floating)
+//#define SHT30_ADDRESS 0x45   //Use if ADDR pin is tied to VCC
+
+// I²C LCD + SHT30/31 will use A4 (SDA) and A5 (SCL)
+
+// === LCD object ===
+LiquidCrystal_I2C lcd(0x27, 16, 2); // Change to 0x3F if needed
+
+
 void setup() {
   // put your setup code here, to run once:
 
